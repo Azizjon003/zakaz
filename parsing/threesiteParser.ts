ee5dd9a1024dc11394faad667ef5ac8ba4d9bb40
azizjon <aliqulovazizjon68@gmail.com>
2022-09-23 10:07:19 +0500

    update qismi tugatildi

--- parsing/threesiteParser.ts
+++ parsing/threesiteParser.ts
@@ -8,18 +8,20 @@ interface Data {
   description: string; //bu ham url bo'ladi faqat to'liqroq
   date: number; //bu qachon maqola chiqarilganligi haqida
 }
-let getData: (url: string) => void = async (url) => {
+let getData = async (url: string, num: number) => {
+  let mainUrl = "https://cointelegraph.com/news/";
   const data = await axios.post(url, {
     operationName: "MainPagePostsQuery",
     variables: {
-      offset: 0,
-      length: 20,
+      offset: (num - 1) * 15,
+      length: num * 15,
       short: "en",
       cacheTimeInMS: 1000,
     },
     query:
       'query MainPagePostsQuery($short: String, $offset: Int!, $length: Int!, $place: String = "index") {\n  locale(short: $short) {\n    posts(\n      order: "postPublishedTime"\n      offset: $offset\n      length: $length\n      place: $place\n    ) {\n      data {\n        cacheKey\n        id\n        slug\n        postTranslate {\n          cacheKey\n          id\n          title\n          avatar\n          published\n          publishedHumanFormat\n          leadText\n          author {\n            cacheKey\n            id\n            slug\n            authorTranslates {\n              cacheKey\n              id\n              name\n            }\n          }\n        }\n        category {\n          cacheKey\n          id\n          slug\n          categoryTranslates {\n            cacheKey\n            id\n            title\n          }\n        }\n        author {\n          cacheKey\n          id\n          slug\n          authorTranslates {\n            cacheKey\n            id\n            name\n          }\n        }\n        postBadge {\n          cacheKey\n          id\n          label\n          postBadgeTranslates {\n            cacheKey\n            id\n            title\n          }\n        }\n      }\n      postsCount\n      hasMorePosts\n    }\n  }\n}\n',
   });
+  console.log(data.data);
 
   const malumot: {
     data: {
@@ -48,12 +50,17 @@ let getData: (url: string) => void = async (url) => {
   for (let i = 0; i < parser.length; i++) {
     let obj = {} as Data;
     obj.title = parser[i].postTranslate.title;
-    obj.description = parser[i].postTranslate.leadText;
+    obj.description = mainUrl + parser[i].slug;
     obj.date = new Date(parser[i].postTranslate.published).getTime();
     obj.imageUrl = parser[i].postTranslate.avatar;
     arr.push(obj);
   }
   console.log(arr);
+  return arr;
 };
-getData("https://graphcdn.cointelegraph.com/");
-export { getData };
+const getParserThree = async (num: number) => {
+  let datt = await getData("https://graphcdn.cointelegraph.com/", num);
+  return datt;
+};
+
+export { getParserThree };
--- utility/bazaUpdate.ts
+++ utility/bazaUpdate.ts
@@ -1,5 +1,6 @@
 import { getData } from "../parsing/twositeParser";
 import { getParser } from "../parsing/onesiteParser";
+import { getParserThree } from "../parsing/threesiteParser";
 import cli from "cli-color";
 const db = require("../model/index");
 const news = db.news;
@@ -111,9 +112,66 @@ const updateOneParser = async (model: any) => {
     console.log(cli.blue(`${j + 1} eleamnt bazaga qo'shildi => ${son}`));
   }
 };
+const updateThreeParser = async (model: any) => {
+  // const data = await model.findAll({
+  //   where: {
+  //     turi: "3",
+  //   },
+  //   order: [["date", "DESC"]],
+  // });
+  // const test: number = data[0].dataValues.date;
+  const test = new Date().getTime() - 5 * 60 * 60 * 1000;
+  let shart: boolean = true;
+  let son: number = 1;
+  let mainArr: {
+    title: string;
+    imageUrl: string;
+    description: string;
+    date: number;
+    turi?: string;
+  }[] = [];
+
+  while (shart) {
+    let arr: {
+      title: string;
+      imageUrl: string;
+      description: string;
+      date: number;
+      turi?: string;
+    }[] = await getParserThree(son);
+    let arr2: {
+      title: string;
+      imageUrl: string;
+      description: string;
+      date: number;
+      turi?: string;
+    }[] = arr.sort((a, b) => {
+      return b.date - a.date;
+    });
+    if (arr2[0].date > test) {
+      for (let i = 0; i < arr2.length; i++) {
+        if (arr2[i].date > test) {
+          arr2[i].turi = "3";
+          mainArr.push(arr2[i]);
+        }
+      }
+    } else {
+      shart = false;
+    }
+    son++;
+  }
+  // console.log(mainArr);
+  for (let j = 0; j < mainArr.length; j++) {
+    console.log(cli.red("e Xurshid kalla create"));
+    console.log(mainArr[j]);
+    let son = await model.create(mainArr[j]);
+    console.log(cli.blue(`${j + 1} eleamnt bazaga qo'shildi => ${son}`));
+  }
+};
 
 const ishla = async () => {
   // updateTwoParser(news);
-  updateOneParser(news);
+  // updateOneParser(news);
+  updateThreeParser(news);
 };
 ishla();
